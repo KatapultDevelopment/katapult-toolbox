eaf8cc42506975b21ede0fda3f4333f93dbc3392
Avery deGruchy <averydegruchy@gmail.com>
2022-05-12 18:32:55 -0400

    Fixed naming and scoping of modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as Functions from './katapult-functions.js';
 import * as Geometry from './katapult-geometry.js';
 
-export const KatapultFunctions = { Geometry };
-export const KatapultGeometry = { Functions };
+export const KatapultFunctions = { ...Functions };
+export const KatapultGeometry = { ...Geometry };
